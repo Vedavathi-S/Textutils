56f09955495b7b56a8418f5a4cf957ac0d978355
Vedavathi-S <vedavathivedavathi18@gmail.com>
2025-07-28 20:39:21 +0530

    learnt props

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+import person,{b,s} from './module2.mjs';
+
+
+console.log(person); // Should log "veda"
+console.log(b);
+console.log(s);
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+const a = "veda";
+const b = "bhavana";
+const s = "sudha";
+
+export default a;
+export {b,s};
+
