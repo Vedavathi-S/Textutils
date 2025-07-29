916e1a882d381f6538e5d343c925d3c084d7a463
Vedavathi-S <vedavathivedavathi18@gmail.com>
2025-07-29 21:21:46 +0530

    added logics to textUtils

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Navbar(props) {
-  //  if (typeof props.title !== 'string') {
-  //   console.error(`PropType Warning: Invalid prop 'title' of type '${typeof props.title}' supplied to 'Navbar', expected 'string'.`);
-  // }
-  // if (typeof props.aboutText !== 'string') {
-  //   console.error(`PropType Warning: Invalid prop 'aboutText' of type '${typeof props.aboutText}' supplied to 'Navbar', expected 'string'.`);
-  // }
+   if (typeof props.title !== 'string') {
+    console.error(`PropType Warning: Invalid prop 'title' of type '${typeof props.title}' supplied to 'Navbar', expected 'string'.`);
+  }
+  if (typeof props.aboutText !== 'string') {
+    console.error(`PropType Warning: Invalid prop 'aboutText' of type '${typeof props.aboutText}' supplied to 'Navbar', expected 'string'.`);
+  }
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,7 +28,7 @@ function Navbar(props) {
           </ul>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <button className="btn btn-outline-primary" type="submit">Search</button>
           </form>
         </div>
       </div>
@@ -39,10 +39,10 @@ function Navbar(props) {
 Navbar.propTypes={title:PropTypes.string.isRequired,
                 aboutText:PropTypes.string.isRequired
 }
-// Navbar.defaultProps={
-//   title:'set title here',
-//   aboutText:'about text here'
-// };
+Navbar.defaultProps={
+  title:'set title here',
+  aboutText:'about text here'
+};
 
 export default Navbar;
 
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -3,26 +3,53 @@ import React,{useState} from 'react'
 
 
 export default function Textform(props) {
-    const [text, setText]=useState('Enter text here');
+    const [text, setText]=useState('');
     const handleUpClick=()=>{
         // console.log('uppercase was clicked'+text);
         let newtxt=text.toUpperCase();
-        setText(newtxt)
+        setText(newtxt);
+    }
+    const handlelowClick=()=>{
+      let newtxt=text.toLowerCase();
+      setText(newtxt);
+    }
+    const handleclear=()=>{
+      let newtxt=" "
+      setText(newtxt);
+    }
+    const handleSpeak=()=>{
+      window.speechSynthesis.cancel();
+      let msg=new SpeechSynthesisUtterance(text);
+      //msg.text=text;
+      window.speechSynthesis.speak(msg);
     }
     const handleOnChange=(event)=>{
         // console.log('onChange is pressed');
         setText(event.target.value);
     }
-  return (
     
-    <div>
+  return (
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         
         <textarea className="form-control" id="mybox" value={text} onChange={handleOnChange} rows="8"></textarea>
         
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick} >Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick} >Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handlelowClick}>LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleclear}>ClearText</button>
+        <button className="btn btn-primary mx-2" type="submit" onClick={handleSpeak}>Speak</button>
     </div>
+    <div className="container my-3" >
+    <h2>Text Summary</h2>
+    <p>{text.split(" ").length } words, {text.length} characters</p>
+    <p>{0.08*text.split(" ").length} Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
+
+    </div>
+    </>
   )
 }
