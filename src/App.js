6e2c994f90b2ffe5a773fb1d7d73882df12cbe35
Vedavathi-S <vedavathivedavathi18@gmail.com>
2025-08-03 23:34:03 +0530

    Added React router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@
 
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Alert from './components/Alert';
 import Textform from './components/Textform';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 // console.warn("PropTypes warning test");
 
 
@@ -35,38 +40,45 @@ function App() {
       document.body.style.backgroundColor='#042743';
       document.body.style.color = 'white';
       showalert('Enabled Dark mode','success');
+      // setInterval(()=>{
+      //   document.title='Install now'
+      // },2000);
+      // setInterval(()=>{
+      //   document.title='Now itself'
+      // },1000);  for flashing msg in a title
     }
   }
   const toggleSmode=()=>{
     if(smode==='light')
     {
-      setSmode('dark');
-      document.body.style.backgroundolor='green';
+      setSmode('green');
+      document.body.style.backgroundColor='green';
       document.body.style.color='white'
     }
     else
     {
       setSmode('light');
-      document.body.style.backgroundolor='white';
-      document.body.style.color='black';
+      document.body.style.backgroundColor='white';
+      document.body.style.color='black'
     }
   };
-  
+
   return (
     <>
-      {/* Pass wrong types intentionally */}
+    <Router>
       <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} smode={smode} toggleSmode={toggleSmode}/>
-      <div style={{ height: '50px' }}>
+    <div style={{ height: '50px' }}>
       <Alert alert={alert} />
     </div>
 
-      <div className="container">
-        <Textform heading="Enter your text to analyze below" showalert={showalert} mode={mode} />
-      </div>
+    <div className="container my-3">
+      <Routes>
+        <Route exact path="/" element={<Textform heading="Enter your text to analyze below" showalert={showalert} mode={mode} />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </div>
+    </Router>
 
-      {/* <div className="container my-5">
-        <About />
-      </div> */}
       
     </>
   );
